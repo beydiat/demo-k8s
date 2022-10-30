ba8e3f0108037064807cfb2a05d8ac70c2d50cf9
beydi <beydibat@yahoo.fr>
2022-10-30 15:34:34 +0100

    complete and fix mongo service

--- launch-scripts.sh
+++ launch-scripts.sh
@@ -1,1 +1,10 @@
-kubectl create deployment kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1
+# Creation of mongodb, mongo-express
+kubectl apply -f mongo/mongo-secret.yaml
+kubectl apply -f mongo/mongo.yaml
+kubectl apply -f mongo/mongo-configmap.yaml
+kubectl apply -f mongo/mongo-express.yaml
+# Enabling ingress on minikube
+minikube addons enable ingress
+# Excposition of mongo-express service
+kubectl expose deployment mongo-express --type=NodePort --port=8081
+minikube service mongo-express --url
